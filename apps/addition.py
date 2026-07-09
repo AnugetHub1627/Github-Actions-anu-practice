958123d7c42b9a268f894b1e1242c976d255a5b5
AnugetHub1627 <anummore27@gmail.com>
2026-07-09 00:19:34 -0700

    Create addition.py

---
+++ apps/addition.py
@@ -0,0 +1,6 @@
+# this is simple python program for addition of 2 numbers just to check how github actions works
+def add(a,b):
+  return a + b
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
