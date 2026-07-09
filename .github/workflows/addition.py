2e4057ae235fd254bc8accef633d38e48e680256
AnugetHub1627 <anummore27@gmail.com>
2026-07-09 00:18:50 -0700

    Delete .github/workflows/addition.py

--- .github/workflows/addition.py
+++
@@ -1,6 +0,0 @@
-# this is simple python program for addition of 2 numbers just to check how github actions works
-def add(a,b):
-  return a + b
-def test_add():
-    assert add(1, 2) == 3
-    assert add(1, -1) == 0
